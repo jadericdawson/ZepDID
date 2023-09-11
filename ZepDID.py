4d460aad7c7c05e3434126991b01881ae5867f7b
jadericdawson <jadericdawson@gmail.com>
2023-09-11 16:06:00 -0400

    Add files via upload

--- ZepDID.py
+++ ZepDID.py
@@ -2,8 +2,7 @@ import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget, QPushButton, QTextEdit, QLineEdit, QFileDialog
 from PyQt5.QtCore import Qt, QThreadPool, QRunnable
 from PyQt5.QtGui import QFont
-from pydantic import Field
-from os import environ
+from pydantic.v1 import BaseModel, Field, validator
 from langchain.tools import BaseTool
 from langchain.chat_models import ChatOpenAI
 from langchain.agents import Tool, AgentType, load_tools, initialize_agent
@@ -22,10 +21,15 @@ from uuid import uuid4
 import getpass
 import time
 from langchain.memory import CombinedMemory, VectorStoreRetrieverMemory
-
-
+from typing import Optional, Type
+from langchain.tools import MoveFileTool, format_tool_to_openai_function
+from langchain.callbacks.manager import (
+    AsyncCallbackManagerForToolRun,
+    CallbackManagerForToolRun,
+)
+from os import environ
 # Set OpenAI API key
-environ["OPENAI_API_KEY"] = "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"  #"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"
+environ["OPENAI_API_KEY"] = "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"
 
 # Set this to your Zep server URL
 ZEP_API_URL = "http://localhost:8000"
@@ -42,9 +46,30 @@ zep_memory = ZepMemory(
 
 
 llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0613")
+math_llm = OpenAI(temperature=0.0)
 
 files = []
 tools = []
+
+class PDF_Tools(BaseModel):
+    question: str = Field()
+
+
+'''class CustomSearchTool(BaseModel):
+    name = "custom_search"
+    description = "useful for when you need to answer questions about current events"
+
+    def _run(
+        self, query: str, run_manager: Optional[CallbackManagerForToolRun] = None
+    ) -> str:
+        """Use the tool."""
+        return search.run(query)
+
+    async def _arun(
+        self, query: str, run_manager: Optional[AsyncCallbackManagerForToolRun] = None
+    ) -> str:
+        """Use the tool asynchronously."""
+        raise NotImplementedError("custom_search does not support async")'''
 
 class OpenFilesRunnable(QRunnable):
     def __init__(self, mainWindow, files_paths):
@@ -59,7 +84,7 @@ class OpenFilesRunnable(QRunnable):
         for path in self.files_paths:
             file_name = path.split("/")[-1].split(".")[0]
             files.append({
-                "name": file_name,
+                "name": file_name, 
                 "path": path
             })
         for file in files:
@@ -69,19 +94,20 @@ class OpenFilesRunnable(QRunnable):
             docs = text_splitter.split_documents(pages)
             embeddings = OpenAIEmbeddings()
             retriever = FAISS.from_documents(docs, embeddings).as_retriever()
+            print(file)
             
             tools.append(
                 Tool(
-                    args_schema=None,
-                    name=file["name"],
+                    args_schema=PDF_Tools,
+                    name=file['name'],
                     description=f"useful when you want to answer questions about {file['name']}. Also known as a document.",
                     func=RetrievalQA.from_chain_type(llm=llm, retriever=retriever),
                 )
             )
-        print(files)
-        math_llm = OpenAI(temperature=0.0)
-        tools_new = load_tools(["human", "llm-math"], llm=math_llm)
-        tools = tools + tools_new
+        
+
+        
+
         agent_kwargs = {
             "system_message": SystemMessage(content="You are a government engineer tasked with searching existing Data Item Descriptions (DID) to determine if duplicates exist. Review the DIDs, summarize each, and give a final opinion of the similarities, differences, and recommend if one or the other can be removed from the database.")
         }
@@ -101,10 +127,12 @@ class OpenFilesRunnable(QRunnable):
                              agent_kwargs=agent_kwargs,
                              memory=memory,)
 
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.initUI()
+        self.initializeAgent()  # Initialize the chat agent
         self.files = []  # Initialize an empty files list
 
     def initUI(self):
@@ -137,12 +165,37 @@ class MainWindow(QMainWindow):
         self.openFilesButton.clicked.connect(self.onOpenFiles)
         layout.addWidget(self.openFilesButton)
 
+
     def onOpenFiles(self):
         files_paths, _ = QFileDialog.getOpenFileNames(self, "Open PDF Files", "", "PDF Files (*.pdf)")
         if files_paths:
             openFilesRunnable = OpenFilesRunnable(self, files_paths)
             QThreadPool.globalInstance().start(openFilesRunnable)
+    
+    def initializeAgent(self):
+        # Define agent_kwargs and tools as needed
+        agent_kwargs = {
+            "system_message": SystemMessage(content="You are a government engineer tasked with searching existing Data Item Descriptions (DID) to determine if duplicates exist. Review the DIDs, summarize each, and give a final opinion of the similarities, differences, and recommend if one or the other can be removed from the database.")
+        }
+        
+        tools = load_tools(["human", "llm-math"], llm)
 
+        # Set up Zep Chat History
+        memory = ZepMemory(
+            session_id=session_id,
+            url=ZEP_API_URL,
+            api_key=zep_api_key,
+            memory_key="chat_history",
+        )
+        # Initialize the chat agent and assign it to the self.agent attribute
+        self.agent = initialize_agent(
+            agent=AgentType.OPENAI_FUNCTIONS,#OPENAI_MULTI_FUNCTIONS,
+            tools=tools,
+            llm=llm,
+            verbose=True,
+            agent_kwargs=agent_kwargs,
+            memory=memory,
+        )
     def onSend(self):
         user_input = self.inputField.text()
         if user_input:
